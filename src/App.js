fa24c8b5bcd9a6ddec822a9af8186f6c84494046
Muhammad Irfan <m.irfanchamp@gmail.com>
2020-06-12 01:52:24 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world to all</h1>  
+      <h1>hello world to all</h1>
     </div>
   );
 }
